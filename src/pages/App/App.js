1bf57abcf7bfc8a2d00a9a50a82fa9ee82eda1d5
Venessa <gomesvenessa@gmail.com>
2022-10-04 22:23:21 -0400

    basic setup

---
+++ src/pages/App/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import './App.css';
+
+export default class App extends Component {
+  render () {
+  return (
+    <div className="App">
+  <h1>HELLO</h1>
+    </div>
+  );
+}
+}
+
