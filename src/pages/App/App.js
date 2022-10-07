f58945bf064ffb4d1d717e1d1a6352246a399ad9
Venessa <gomesvenessa@gmail.com>
2022-10-07 17:21:42 -0400

    routes done

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,17 @@
 import './App.css'
 import {useState,useEffect} from 'react'
 import {AppContainer} from './App.styled'
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Menu from '../../components/Menu/Menu'
 import AuthPage from '../Auth/AuthPage'
 import Home from '../Homepage/Homepage'
+import AllVendors from '../AllVendors/AllVendors'
+import VendorDetail from '../VendorDetail/VendorDetail'
+import Cart from '../Cart/Cart'
+import AddPackage from '../AddPackage/AddPackage'
+import ViewPackage from '../ViewPackage/ViewPackage'
+import Checkout from '../Checkout/Checkout'
+
 
 
 const App = () => {
@@ -55,8 +62,19 @@ updateUserState()
       <Menu user={user} logout={logout}/>
       <Routes>
       <Route path='/' element={<Home />}/>
+      <Route path='/vendors' element={<AllVendors />} />
+      <Route path='/vendor/:id' element={<VendorDetail />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/package/add' element={<AddPackage />} />
+      <Route path='/package/:id' element={<ViewPackage />} />
+      <Route path='/checkout' element={<Checkout />} />
+
+
+
+
       <Route path='/auth' element={<AuthPage user={user} authUser={authUser} /> }/>
       <Route path="*" element={<Navigate to="/" replace />} />
+      
       </Routes>
     </AppContainer>
   )
---
+++ src/pages/VendorDetail/VendorDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VendorDetail = () => {
+  return (
+    <div>VendorDetail</div>
+  )
+}
+
+export default VendorDetail
