9a6ae205a6aa9e5bf92e0f5281280798315c9fe6
gobinath shanthakumar <gobinathshanthakumar@gobinaths-MacBook-Air.local>
2022-10-10 22:32:24 -0400

    new before first build

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -10,15 +10,30 @@ import {
     LogStatusTitle
 } from './Menu.styled'
 
+// MATERIAL UI ICONS
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge from '@mui/material/Badge';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+
 const Menu = () => {
   const dispatch = useDispatch()
   const {userInfo} = useSelector(state => state.login)
+  const cartData = useSelector(state => state.cartData)
+  const cartNum = cartData.cart.length
   const logout = () => {
     dispatch(logoutUser())
   }
 
+
+
+
+
+
+
+
   return (
     <MenuContainer>
+      {console.log(cartData)}
         <MenuLeft><Link to='/'>EVENT HANDLERS</Link></MenuLeft>
         <MenuMid>{userInfo && userInfo.name ? userInfo.name : ''}</MenuMid>
         <MenuRight>
@@ -28,6 +43,7 @@ const Menu = () => {
         <MenuItem name='Vendors' link='/vendors'/>
         <MenuItem name='Cart' link='/cart'/>
         <MenuItem name='About' link=''/>
+
         </> }
         {userInfo && userInfo.name ? <LogStatusTitle onClick={()=>logout()}>Logout</LogStatusTitle>:<Link to='/auth'><LogStatusTitle>Login</LogStatusTitle></Link>}
         </MenuRight>
--- src/pages/VendorDetail/VendorDetail.styled.jsx
+++ src/pages/VendorDetail/VendorDetail.styled.jsx
@@ -1,2 +1,60 @@
 import styled from 'styled-components'
 import {motion }from 'framer-motion'
+
+export const VDetailContainer = styled(motion.div)`
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    margin-top:10vh;
+
+`
+
+export const VDetailUser = styled(motion.div)`
+    width:80vw;
+    display:flex;
+    flex-direction:column;
+    border: solid 1px black;
+    padding:20px;
+    margin:20px;
+`
+
+export const TopUserInfo = styled(motion.div)`
+    display:flex;
+    flex-direction:row;
+    border: solid 1px black;
+    justify-content: space-around;
+`
+
+export const BottomUserImages = styled(motion.div)`
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    margin-top:10vh;
+    border: solid 1px black;
+`
+
+
+export const ImageView = styled(motion.img)`
+width:100px;
+height:100px;
+`
+
+
+export const UserInfoLeft = styled(motion.div)`
+display:flex;
+flex-direction:column;
+
+`
+
+export const UserInfoMid = styled(motion.div)`
+display:flex;
+flex-direction:column;
+`
+
+
+export const UserInfoRight = styled(motion.div)`
+display:flex;
+flex-direction:column;
+`
