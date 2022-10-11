ebfb8c503838e220fa0d56efd6ab233eb9491e99
Louis Cheung <louis.cheung.2@gmail.com>
2022-10-10 23:33:46 -0400

    merge to main

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const app = express();
+const cors = require('cors');
 
 
 require('dotenv').config();
@@ -10,6 +11,7 @@ require('./config/database');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
+//socket-io imports
+// import * as serviceWorker from './serviceWorker';
+import io from 'socket.io-client'
+let socket = io("http://192.168.1.53:3000");
+socket.on('message from server', function(msg){
+alert(msg);
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
