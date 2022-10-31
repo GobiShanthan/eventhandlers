430fa62d0f772e9c74f0c93c1da96df7bf22e214
gobinath shanthakumar <gobinathshanthakumar@gobinaths-MacBook-Air.local>
2022-10-31 02:15:26 -0400

    before final fix

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -7,14 +7,20 @@ import {getStorage} from 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-  appId:process.env.REACT_APP_FIREBASE_APP_ID 
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  // appId:process.env.REACT_APP_FIREBASE_APP_ID 
+  apiKey: "AIzaSyBlq2Q6Aiu5ZD0fqzGXJt0x7MpU7V5gm90",
+  authDomain: "eventhandlers-29ea1.firebaseapp.com",
+  projectId: "eventhandlers-29ea1",
+  storageBucket: "eventhandlers-29ea1.appspot.com",
+  messagingSenderId: "87563983877",
+  appId: "1:87563983877:web:4bd4ef11a4262dcb68506c"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage()
+export const storage = getStorage(app)
